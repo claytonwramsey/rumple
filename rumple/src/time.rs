73f61a3ecb206f4c18cd2848c8fd78fbcf427e79
Clayton Ramsey <claytonwramsey@gmail.com>
2024-09-15 04:33:44 -0500

    expand variety of timeouts

--- rumple/src/time.rs
+++ rumple/src/time.rs
@@ -1,7 +1,102 @@
 use crate::Timeout;
 
+/// A helper structure for generating a composite timeout of multiple conditions.
+///
+/// # Examples
+///
+/// ```
+/// use rumple::{Timeout, time::{Forever, LimitSamples, Any}};
+/// let tc0 = Forever;
+/// let tc1 = LimitSamples::new(1000);
+/// let composed = Any((tc0, tc1));
+/// assert!(!composed.is_over());
+/// ```
+///
+/// You can use the bitwise-or operator on any provided timeout for easy composition.
+///
+/// ```
+/// use rumple::{Timeout, time::{LimitSamples, LimitNodes, Any}};
+/// let tc0 = LimitNodes::new(100);
+/// let tc1 = LimitSamples::new(1000);
+/// let composed = tc0 | tc1;
+/// assert!(!composed.is_over());
+/// ```
+pub struct Any<T>(pub T);
+
+pub struct Forever;
+
+#[cfg(feature = "std")]
+pub use alarm::Alarm;
+
+pub struct LimitSamples {
+    current: usize,
+    limit: usize,
+}
+
+pub struct LimitNodes {
+    current: usize,
+    limit: usize,
+}
+
+/// Implement Timeout for an Any of some tuple.
+macro_rules! any_tuple {
+    () => {}; // This case is handled above by the trivial case
+    ($($args:ident),*) => {
+        impl<$($args: Timeout),*> Timeout for Any<($($args,)*)> {
+            fn is_over(&self) -> bool {
+                #[allow(non_snake_case)]
+                let &($(ref $args,)*) = &self.0;
+                $(
+                    if ($args).is_over() {
+                        return true;
+                    }
+                )*
+                false
+            }
+
+            fn update_sample_count(&mut self, n: usize) {
+                #[allow(non_snake_case)]
+                let &mut ($(ref mut $args,)*) = &mut self.0;
+                $(
+                    $args.update_sample_count(n);
+                )*
+            }
+
+
+            fn update_node_count(&mut self, n: usize) {
+                #[allow(non_snake_case)]
+                let &mut ($(ref mut $args,)*) = &mut self.0;
+                $(
+                    $args.update_node_count(n);
+                )*
+            }
+        }
+    }
+}
+
+any_tuple!();
+any_tuple!(A);
+any_tuple!(A, B);
+any_tuple!(A, B, C);
+any_tuple!(A, B, C, D);
+any_tuple!(A, B, C, D, E);
+any_tuple!(A, B, C, D, E, F);
+any_tuple!(A, B, C, D, E, F, G);
+
+macro_rules! bitor_impl {
+    ($t: ident) => {
+        impl<R: Timeout> core::ops::BitOr<R> for $t {
+            type Output = Any<($t, R)>;
+            fn bitor(self, rhs: R) -> Self::Output {
+                Any((self, rhs))
+            }
+        }
+    };
+}
+
 #[cfg(feature = "std")]
 mod alarm {
+    use super::Any;
     use crate::Timeout;
     use core::time::Duration;
     use std::time::Instant;
@@ -32,6 +127,8 @@ mod alarm {
         }
     }
 
+    bitor_impl!(Alarm);
+
     #[cfg(test)]
     mod tests {
         use super::*;
@@ -46,13 +143,52 @@ mod alarm {
     }
 }
 
-#[cfg(feature = "std")]
-pub use alarm::Alarm;
-
-pub struct Forever;
-
 impl Timeout for Forever {
     fn is_over(&self) -> bool {
         false
     }
 }
+
+impl LimitNodes {
+    #[must_use]
+    pub const fn new(n: usize) -> Self {
+        Self {
+            current: 0,
+            limit: n,
+        }
+    }
+}
+
+impl LimitSamples {
+    #[must_use]
+    pub const fn new(n: usize) -> Self {
+        Self {
+            current: 0,
+            limit: n,
+        }
+    }
+}
+
+impl Timeout for LimitNodes {
+    fn is_over(&self) -> bool {
+        self.current >= self.limit
+    }
+
+    fn update_node_count(&mut self, n: usize) {
+        self.current += n;
+    }
+}
+
+impl Timeout for LimitSamples {
+    fn is_over(&self) -> bool {
+        self.current >= self.limit
+    }
+
+    fn update_sample_count(&mut self, n: usize) {
+        self.current += n;
+    }
+}
+
+bitor_impl!(Forever);
+bitor_impl!(LimitSamples);
+bitor_impl!(LimitNodes);
