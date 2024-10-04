f6270268c23aaab291481a9df66aace794bae007
Clayton Ramsey <claytonwramsey@gmail.com>
2024-10-04 14:17:13 -0500

    the problem was my kd tree

--- examples/src/bin/geo_car.rs
+++ examples/src/bin/geo_car.rs
@@ -12,23 +12,24 @@ use rumple::{
     space::{Angle, Pose2d, PoseRadius, Vector, WeightedPoseDistance},
     time::Solved,
     valid::SampleValidate,
+    Metric, NearestNeighborsMap,
 };
 
 fn main() {
     let mut env = World2d::new();
-    // // left bug trap
-    // env.add_aabb(0.0, 0.0, 4.0, 1.0);
-    // env.add_aabb(0.0, 1.0, 1.0, 4.0);
-    // env.add_aabb(3.0, 1.0, 4.0, 8.0);
-    // env.add_aabb(0.0, 5.0, 1.0, 9.0);
-    // env.add_aabb(1.0, 8.0, 4.0, 9.0);
+    // left bug trap
+    env.add_aabb(0.0, 0.0, 4.0, 1.0);
+    env.add_aabb(0.0, 1.0, 1.0, 4.0);
+    env.add_aabb(3.0, 1.0, 4.0, 8.0);
+    env.add_aabb(0.0, 5.0, 1.0, 9.0);
+    env.add_aabb(1.0, 8.0, 4.0, 9.0);
 
-    // // right bug trap
-    // env.add_aabb(5.0, 0.0, 9.0, 1.0);
-    // env.add_aabb(8.0, 1.0, 9.0, 4.0);
-    // env.add_aabb(5.0, 1.0, 6.0, 8.0);
-    // env.add_aabb(5.0, 8.0, 9.0, 9.0);
-    // env.add_aabb(8.0, 5.0, 9.0, 8.0);
+    // right bug trap
+    env.add_aabb(5.0, 0.0, 9.0, 1.0);
+    env.add_aabb(8.0, 1.0, 9.0, 4.0);
+    env.add_aabb(5.0, 1.0, 6.0, 8.0);
+    env.add_aabb(5.0, 8.0, 9.0, 9.0);
+    env.add_aabb(8.0, 5.0, 9.0, 8.0);
 
     let start = Pose2d {
         position: Vector([2.0, 3.0].map(r64)),
@@ -62,17 +63,21 @@ fn main() {
     );
 
     let grow_radius = PoseRadius {
-        angle_dist: r64(PI / 180.0),
-        position_dist: r64(0.1),
+        angle_dist: r64(PI / 5.0),
+        position_dist: r64(2.0),
     };
     let mut rrt = Rrt::new(
         start,
-        KdTreeMap::new(WeightedPoseDistance {
-            position_metric: SquaredEuclidean,
-            position_weight: r64(1.0),
-            angle_metric: SquaredEuclidean,
-            angle_weight: r64(1.0),
-        }),
+        BruteForce {
+            poses: Vec::new(),
+            values: Vec::new(),
+            metric: WeightedPoseDistance {
+                position_metric: SquaredEuclidean,
+                position_weight: r64(1.0),
+                angle_metric: SquaredEuclidean,
+                angle_weight: r64(1.0),
+            },
+        },
         &valid,
     );
     let traj = rrt
@@ -91,4 +96,34 @@ fn main() {
 
     println!("Created {} nodes", rrt.num_nodes());
     println!("{traj:?}");
+}
+
+struct BruteForce<V, M> {
+    poses: Vec<Pose2d<R64>>,
+    values: Vec<V>,
+    metric: M,
+}
+
+impl<V, M> NearestNeighborsMap<Pose2d<R64>, V> for BruteForce<V, M>
+where
+    M: Metric<Pose2d<R64>>,
+{
+    fn insert(&mut self, key: Pose2d<R64>, value: V) {
+        self.poses.push(key);
+        self.values.push(value);
+    }
+
+    fn nearest<'q>(&'q self, key: &Pose2d<R64>) -> Option<(&'q Pose2d<R64>, &'q V)> {
+        let mut best_i = 0;
+        let mut best_dist = self.metric.distance(self.poses.first()?, key);
+        for (i, pose) in self.poses.iter().enumerate() {
+            let dist = self.metric.distance(pose, key);
+            if dist < best_dist {
+                best_i = i;
+                best_dist = dist;
+            }
+        }
+
+        Some((&self.poses[best_i], &self.values[best_i]))
+    }
 }
