f7a000988dd9611cbd1ef58f0ba6f674815e8846
Clayton Ramsey <claytonwramsey@gmail.com>
2024-10-12 00:52:37 -0500

    simd parallel benchmarks for geo_maze3d

--- benches/geo_maze3d.rs
+++ benches/geo_maze3d.rs
@@ -1,10 +1,11 @@
+#![cfg_attr(feature = "simd", feature(portable_simd))]
+
 use core::hint::black_box;
 use rand::SeedableRng;
 use rand_chacha::ChaCha20Rng;
 use rumple::{
     env::World3d,
-    geo::rrt_connect,
-    geo::Prm,
+    geo::{rrt_connect, Prm},
     metric::SquaredEuclidean,
     nn::KdTreeMap,
     sample::Rectangle,
@@ -13,11 +14,60 @@ use rumple::{
     valid::{GeoValidate, SampleInterpolate},
     Sample,
 };
+#[cfg(feature = "simd")]
+use rumple::{valid::Validate, Metric};
+#[cfg(feature = "simd")]
+use std::{array, simd::Simd};
 
-use brunch::Bench;
+use brunch::{Bench, Benches};
+
+type F = f32;
+
+#[cfg(feature = "simd")]
+const L: usize = 8;
+
+#[cfg(feature = "simd")]
+struct RakeValidate<'a> {
+    radius: F,
+    rake_width: F,
+    env: &'a World3d<F>,
+}
+
+#[cfg(feature = "simd")]
+impl<'a> Validate<Vector<3, F>> for RakeValidate<'a> {
+    fn is_valid_configuration(&self, &Vector([x, y, z]): &Vector<3, F>) -> bool {
+        !self.env.collides_ball(x, y, z, self.radius)
+    }
+}
+
+#[cfg(feature = "simd")]
+impl<'a> GeoValidate<Vector<3, F>> for RakeValidate<'a> {
+    fn is_valid_transition(&self, v0: &Vector<3, F>, v1: &Vector<3, F>) -> bool {
+        let distsq = SquaredEuclidean.distance(v0, v1);
+        let rakesq = self.rake_width;
+        let start_offsets =
+            Simd::<F, L>::from_array(array::from_fn(|i| i as F)) / Simd::splat(L as F);
+        let rake_frac = rakesq / distsq;
+        let [x_add, y_add, z_add] = array::from_fn(|i| Simd::splat((v1[i] - v0[i]) * rake_frac));
+        let n_steps = (rake_frac * L as F).recip().ceil() as u32;
+        let [mut x, mut y, mut z] =
+            array::from_fn(|i| Simd::splat(v0[i]) + Simd::splat(v1[i] - v0[i]) * start_offsets);
+        let rs = Simd::splat(self.radius);
+        // println!("{n_steps}");
+        for _ in 0..n_steps {
+            if self.env.collides_balls(x, y, z, rs) {
+                return false;
+            }
+            x += x_add;
+            y += y_add;
+            z += z_add;
+        }
+        true
+    }
+}
 
 fn main() {
-    let h = 1.0;
+    let h: F = 1.0;
     let start = Vector([2.5, 5.0, h / 2.0]);
     let goal = Vector([2.5, 0.0, h / 2.0]);
     let mut env = World3d::new();
@@ -26,7 +76,7 @@ fn main() {
     let h = 1.0;
     let wall_hw = 0.05;
 
-    let mut add_vert = |x, start_y, end_y| {
+    let mut add_vert = |x: F, start_y: F, end_y: F| {
         env.add_aabb(x - wall_hw, start_y, 0.0, x + wall_hw, end_y, 1.0);
     };
 
@@ -49,9 +99,10 @@ fn main() {
     add_horz(5.0, 0.0, 2.0);
     add_horz(5.0, 3.0, 5.0);
 
+    let step_size = 0.1 * 0.1; // measured in distsq
     let valid = SampleInterpolate::new(
-        |&Vector([x, y, z]): &Vector<3>| !env.collides_ball(x, y, z, r),
-        0.02,
+        |&Vector([x, y, z]): &Vector<3, F>| !env.collides_ball(x, y, z, r),
+        step_size,
     );
 
     let sampler = Rectangle {
@@ -59,22 +110,77 @@ fn main() {
         max: Vector([5.0, 5.0, 0.5]),
     };
 
-    brunch::benches!(
-        inline:
-        Bench::new("geo_maze3d_prm").run(|| black_box(prm_bench(start, goal, &valid, &sampler, &mut ChaCha20Rng::seed_from_u64(2707)))),
-        Bench::new("geo_maze3d_rrtc").run(|| black_box(rrt_connect(start, goal, KdTreeMap::new(SquaredEuclidean), &valid, &sampler, 1.0, &mut Solved::new(), &mut ChaCha20Rng::seed_from_u64(2707))))
-    )
+    let mut benches = Benches::default();
+    benches.push(Bench::new("geo_maze3d_prm").run(|| {
+        black_box({
+            black_box(prm_bench(
+                black_box(start),
+                black_box(goal),
+                black_box(&valid),
+                black_box(&sampler),
+                &mut ChaCha20Rng::seed_from_u64(2707),
+            ))
+        })
+    }));
+    benches.push(Bench::new("geo_maze3d_rrtc").run(|| {
+        black_box({
+            black_box(rrt_connect(
+                black_box(start),
+                black_box(goal),
+                KdTreeMap::new(SquaredEuclidean),
+                black_box(&valid),
+                black_box(&sampler),
+                black_box(1.0),
+                &mut Solved::new(),
+                &mut ChaCha20Rng::seed_from_u64(2707),
+            ))
+        })
+    }));
+
+    #[cfg(feature = "simd")]
+    {
+        let rake_valid = RakeValidate {
+            radius: r,
+            rake_width: step_size,
+            env: &env,
+        };
+        benches.push(Bench::new("geo_maze3d_prm_simd").run(|| {
+            black_box({
+                black_box(prm_bench(
+                    black_box(start),
+                    black_box(goal),
+                    black_box(&rake_valid),
+                    black_box(&sampler),
+                    &mut ChaCha20Rng::seed_from_u64(2707),
+                ))
+            })
+        }));
+        benches.push(Bench::new("geo_maze3d_rrtc_simd").run(|| {
+            black_box(rrt_connect(
+                black_box(start),
+                black_box(goal),
+                KdTreeMap::new(SquaredEuclidean),
+                black_box(&rake_valid),
+                black_box(&sampler),
+                black_box(1.0),
+                &mut Solved::new(),
+                &mut ChaCha20Rng::seed_from_u64(2707),
+            ))
+        }));
+    }
+
+    benches.finish();
 }
 
-fn prm_bench<V: GeoValidate<Vector<3>>, S, R>(
-    start: Vector<3>,
-    goal: Vector<3>,
+fn prm_bench<V: GeoValidate<Vector<3, F>>, S, R>(
+    start: Vector<3, F>,
+    goal: Vector<3, F>,
     valid: &V,
     sampler: &S,
     rng: &mut R,
-) -> Vec<Vector<3>>
+) -> Vec<Vector<3, F>>
 where
-    S: Sample<Vector<3>, R>,
+    S: Sample<Vector<3, F>, R>,
 {
     let mut prm = Prm::new(KdTreeMap::new(SquaredEuclidean), valid);
     let s = prm.insert_r(start, 0.0).unwrap();
